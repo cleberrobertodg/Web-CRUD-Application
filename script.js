f0fe0e64b3c6daf2f9426007b782f1b6da5eb27c
cleberrobertodg <cleberrobertodg@gmail.com>
2024-09-21 07:28:10 -0300

    Conclusão do crud, com instalação de Edit e Insert

--- script.js
+++ script.js
@@ -14,6 +14,63 @@ function mostrarAlerta(message, className){
     setTimeout(() => document.querySelector(".alert").remove(), 3000)
 }
 
+//Deletar todos os campos
+function deletarCampos () {
+    document.querySelector("#firstName").value = ""
+    document.querySelector("#lastName").value = ""
+    document.querySelector("#matricula").value = ""
+}
+
+//Adicionar dados
+document.querySelector("#student-form").addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    //Pegar valores do form
+    const primeiroNome = document.querySelector("#firstName").value
+    const sobrenome = document.querySelector("#lastName").value
+    const matricula = document.querySelector("#matricula").value
+
+    //Validação
+    if(primeiroNome == "" || sobrenome == "" || matricula == ""){
+        mostrarAlerta("Por favor, preencha todos os campos do formulário!", "danger")
+    } else{
+        if(selectedRow == null){
+            const list = document.querySelector("#student-list")
+            const row = document.createElement("tr")
+
+            row.innerHTML = `
+            <td>${primeiroNome}</td>
+            <td>${sobrenome}</td>
+            <td>${matricula}</td>
+            <td>
+            <a href="#" class="btn btn-warning btn-sm edit">Editar</a>
+            <a href="#" class="btn btn-danger btn-sm delete">Remover</a>
+            `
+            list.appendChild(row)
+            selectedRow = null
+            mostrarAlerta("Aluno adicionado com sucesso!", "success")
+        } else{
+            selectedRow.children[0].textContent = primeiroNome
+            selectedRow.children[1].textContent = sobrenome
+            selectedRow.children[2].textContent = matricula
+            selectedRow = null
+            mostrarAlerta("Informações de aluno atualizadas com sucesso", "info")
+        }
+        deletarCampos()
+    }
+})
+
+//Atualizar dados
+document.querySelector("#student-list").addEventListener("click", (e) => {
+    target = e.target
+    if(target.classList.contains("edit")){
+        selectedRow = target.parentElement.parentElement
+        document.querySelector("#firstName").value = selectedRow.children[0].textContent
+        document.querySelector("#lastName").value = selectedRow.children[1].textContent
+        document.querySelector("#matricula").value = selectedRow.children[2].textContent
+    }
+})
+
 //Deletar dados
 document.querySelector("#student-list").addEventListener("click", (e) =>{
     target = e.target
